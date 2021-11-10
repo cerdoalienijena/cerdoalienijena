50aa4fe9956ac3eb575b62eb600086c2ed13de8d
CostasAndres <93986301+CostasAndres@users.noreply.github.com>
2021-11-10 12:14:07 +0100

    Add files via upload
    añadidos comentarios

--- proyecto/src/proyecto/Proyecto.java
+++ proyecto/src/proyecto/Proyecto.java
@@ -22,12 +22,17 @@ public class Proyecto {
         Scanner sc = new Scanner(System.in);
         Scanner sc1 = new Scanner(System.in);
         
-        
+        //dificultad se asigna dependiendo del curso del usuario
+        //count es un contador de preguntas correctas
+        //operacion se utiliza para saber que opcion se elige
+        //x e y son utilizados para el algoritmo de euclides
+        //exit para poder salir del programa
         int dificultad, count, operacion,x,y;
         String Usuario;
         char exit =' ';
         System.out.println("Nombre de usuario: "); Usuario = sc.nextLine();
         
+        //Menu de el programa
          do {
         System.out.print("Choose your option:\n"
                                     +"1. Sum \n"
@@ -40,7 +45,8 @@ public class Proyecto {
       
         
          
-          
+          //Este switch nos permite seleccionar que curso somos (dificultad) y llamamos a la funcion determinada por el menu anterior
+          //Implemente la dificultad dentro de cada case porque en el case 5 que es el algoritmo de euclides no hay dificultad ya que es una calculadora
             switch (operacion){
             case 1: 
                 System.out.print("Nivel de dificultad por curso(1-4): "); dificultad = sc.nextInt();
@@ -59,6 +65,7 @@ public class Proyecto {
                 Division(dificultad);
                 break;
             case 5:
+                //En el algoritmo de euclides se piden 2 numeros y estos se mandan a la funcion
                 System.out.println("Introduce 2 numeros");
                 x=sc.nextInt();
                 y=sc.nextInt();
@@ -78,7 +85,15 @@ public class Proyecto {
         
        
     }
+    
+    //En cada funcion hay 4 dificultades de 1 de ESO A 4
+    //En cada dificultad se añaden digitos a los numeros
     static int Suma(int dificultad) {
+        //Preguntas es un contador de preguntas
+        //count es un contador de preguntas correctas
+        //En suma se almacena el resultado de la suma
+        //azar1 y azar2 almacena el numero aleatorio en cada dificultad se aumentan los digitos que pueden tener los numeros
+        //respuesta almacena la respuesta del usuario
         int preguntas = 0, count = 0, suma, azar1, azar2, respuesta;
         Scanner sc3 = new Scanner(System.in);
         Random rnd = new Random();//Generador de numeros aleatorios
@@ -95,13 +110,13 @@ public class Proyecto {
                     if (respuesta == suma) {
                         System.out.println("Es correcto");
                         count++;
-//Si es correcta la respuesta sumamso 1 en el contador de preguntas correctas
+//Si es correcta la respuesta sumamos 1 en el contador de preguntas correctas
                     } else {
                         System.out.println("Es incorrecto");
 
                     }
                     preguntas++; //cada vez que hay un ciclo del programa contamos 1 para saber cuantas preguntas hemos preguntado
-                } while (preguntas != 10);
+                } while (preguntas != 10);//Este ciclo no acaba hasta que no haya preguntado 10 veces
                 System.out.println("Tu puntuacion es " + count + " sobre " + preguntas);
                 return count;
             case 2:
@@ -170,6 +185,11 @@ public class Proyecto {
     }
 
     static int Resta(int dificultad) {
+        //Preguntas es un contador de preguntas
+        //count es un contador de preguntas correctas
+        //En resta se almacena el resultado de la suma
+        //azar1 y azar2 almacena el numero aleatorio en cada dificultad se aumentan los digitos que pueden tener los numeros
+        //respuesta almacena la respuesta del usuario
         int preguntas = 0, count = 0, resta, azar1, azar2, respuesta;
         Scanner sc3 = new Scanner(System.in);
         Random rnd = new Random();
@@ -178,6 +198,7 @@ public class Proyecto {
                 do {
                     azar1 = rnd.nextInt(8) + 1;//generamos un numero del 0 al 8 y le sumamos 1 para que nunca salga 0
                     azar2 = rnd.nextInt(8) + 1;
+                    //Este if sirve para que nunca el resultado sea negativo
                     if (azar1 < azar2) {
                         resta = azar2 - azar1;
                         System.out.println(azar2 + " - " + azar1 + " = ");
@@ -278,6 +299,11 @@ public class Proyecto {
         return count;
     }
     static int Multiplicacion(int dificultad){
+        //Preguntas es un contador de preguntas
+        //count es un contador de preguntas correctas
+        //En multiplicacion se almacena el resultado de la suma
+        //azar1 y azar2 almacena el numero aleatorio en cada dificultad se aumentan los digitos que pueden tener los numeros
+        //respuesta almacena la respuesta del usuario
             int preguntas = 0, count = 0, multiplicacion, azar1, azar2, respuesta;
         Scanner sc3 = new Scanner(System.in);
         Random rnd = new Random();//Generador de numeros aleatorios
@@ -368,6 +394,11 @@ public class Proyecto {
          return count;
 }
         static int Division(int dificultad){
+            //Preguntas es un contador de preguntas
+        //count es un contador de preguntas correctas
+        //En division se almacena el resultado de la suma
+        //azar1 y azar2 almacena el numero aleatorio en cada dificultad se aumentan los digitos que pueden tener los numeros
+        //respuesta almacena la respuesta del usuario
              int preguntas = 0, count = 0, division, azar1, azar2, respuesta;
         Scanner sc3 = new Scanner(System.in);
         Random rnd = new Random();//Generador de numeros aleatorios
@@ -375,6 +406,7 @@ public class Proyecto {
         switch (dificultad) {
         case 1:
                 do {
+                    //Este do while funciana que hasta que los 2 numeros no sean divisibles no se se mandan las 2 variables
                     do{
                     azar1 = rnd.nextInt(8) + 1;//generamos un numero del 0 al 8 y le sumamos 1 para que nunca salga 0
                     azar2 = rnd.nextInt(8) + 1;
@@ -474,7 +506,7 @@ public class Proyecto {
         }
         
         static int euclid(int a, int b) {
-                
+                //En esta funcion se hace el algoritmo de euclides
         if(b==0)
            return a;
        else
